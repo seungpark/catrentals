e60d0b132e51793b8f9b0421f5f15ab66e01f5af
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2015-10-20 13:17:21 -0400

    add index action, show action for cats controller

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,9 +2,11 @@ class Cat < ActiveRecord::Base
   validates :birth_date, presence: true
   validates :name, presence: true
   validates :description, presence: true
-  validates :color, inclusion: { in: %w(black, white, red, tan),
+
+  validates :color, inclusion: { in: %w(black white red tan),
       message: "Not a valid color" }, presence: true
-  validates :sex, inclusion: { in: %(M, F),
+
+  validates :sex, inclusion: { in: %w(M F),
       message: "Not a valid sex" }, presence: true
 
   def age
