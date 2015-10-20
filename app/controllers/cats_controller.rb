5e2c4154bda32691df528e99077406dec4b5ae16
App Academy Student <appacademy@Apps-Mac-mini-7.local>
2015-10-20 15:07:54 -0400

    Edit Cat Form

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -10,5 +10,32 @@ class CatsController < ApplicationController
     render :show
   end
 
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    @cat.save
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cat1 = Cat.create!(birth_date: "2000-01-01" , color: "red", name: "Markov", sex: "M", description: "A cat")
+cat2 = Cat.create!(birth_date: "2005-01-01",
+  color: "white",
+  name: "Sennacy",
+  sex: "F",
+  description: "Another cat"
+)
+
+cat3 = Cat.create!(birth_date: "2005-03-17",
+  color: "tan",
+  name: "Eddy",
+  sex: "M",
+  description: "yet another cat"
+)
